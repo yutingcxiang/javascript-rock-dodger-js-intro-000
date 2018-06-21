db2bebe2ee0538a57e92d800522cdb0a2766c789
Christine Xiang <yutingcxiang@gmail.com>
2018-06-21 22:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function createRock(x) {
   GAME.appendChild(rock);
 
   function moveRock() {
-    rock.style.top = `${top += 2}px`
+    rock.style.top = `${top += 2}px`;
     if(checkCollision(rock)) {
        endGame();
     }
