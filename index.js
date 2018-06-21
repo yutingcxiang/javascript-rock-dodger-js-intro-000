2012c9fb3e42bc9f6f0c88a9ae030d238f7f2d5b
Christine Xiang <yutingcxiang@gmail.com>
2018-06-21 22:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function createRock(x) {
   GAME.appendChild(rock);
 
   function moveRock() {
-      rock.style.left = `${x+2}px`
+    rock.style.tops = `${x+2}px`
     if(checkCollision(rock)) {
        endGame();
     }
