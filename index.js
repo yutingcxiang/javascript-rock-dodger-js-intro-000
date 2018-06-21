bb1e0c0b643f2ca20687bf0fd1b2f5930fdc762b
Christine Xiang <yutingcxiang@gmail.com>
2018-06-21 22:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,9 +74,11 @@ function endGame() {
 
 function moveDodger(e) {
   if (e.which === LEFT_ARROW) {
+    e.preventDefault();
     moveDodgerLeft();
   }
   if (e.which === RIGHT_ARROW) {
+    e.preventDefault();
     moveDodgerRight();
   }
 }
